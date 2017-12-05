56c6f72b61297917a60863690ddc07a85ec62587
Pavel <Pavel@binomtech.com>
2017-12-05 17:55:41 +0500

    fix

--- auth_jwt.go
+++ auth_jwt.go
@@ -171,6 +171,7 @@ func (mw *JWTMiddleware) LoginHandler(writer rest.ResponseWriter, request *rest.
 		return
 	}
 
+	writer.Header().Add("Access-Control-Allow-Origin", "*")
 	writer.WriteJson(resultToken{Token: tokenString})
 }
 
@@ -234,15 +235,15 @@ func (mw *JWTMiddleware) RefreshHandler(writer rest.ResponseWriter, request *res
 
 func (mw *JWTMiddleware) unauthorized(writer rest.ResponseWriter) {
 	writer.Header().Set("WWW-Authenticate", "JWT realm="+mw.Realm)
-	rest.Error(writer, "Not Authorized", http.StatusUnauthorized)
+	rest.Error(writer, "Пользователь не авторизован", http.StatusUnauthorized)
 }
 
 func (mw *JWTMiddleware) notUser(writer rest.ResponseWriter) {
 	writer.Header().Set("WWW-Authenticate", "JWT realm="+mw.Realm)
-	rest.Error(writer, "User does not exist", http.StatusUnauthorized)
+	rest.Error(writer, "Пользователя не существует", http.StatusUnauthorized)
 }
 
 func (mw *JWTMiddleware) notPassword(writer rest.ResponseWriter) {
 	writer.Header().Set("WWW-Authenticate", "JWT realm="+mw.Realm)
-	rest.Error(writer, "Invalid password", http.StatusUnauthorized)
+	rest.Error(writer, "Неверный пароль", http.StatusUnauthorized)
 }
